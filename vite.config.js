c559df53bbaaf8316e58e632737dede9a9cf1198
captainmop <captainmop@mop.captainmop>
2026-02-17 17:39:22 -0500

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/1/', // اسم ریپازیتوری تو
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true // برای دیباگ
   }
 })
