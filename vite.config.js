d7fb13bdb57609f9dc0b2df83f799997e99beda1
captainmop <captainmop@mop.captainmop>
2026-02-18 10:37:03 -0500

    fix base path with env

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/1/',  // این خط رو اضافه کن
+  base: process.env.NODE_ENV === 'production' ? '/1/' : '/',
   server: {
     port: 5173,
     open: true
