a135d4b15c31ef0a648fc63ba269e1d015aab7bb
captainmop <captainmop@mop.captainmop>
2026-02-18 16:14:26 -0500

    fix build with assets

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,21 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    // این مهمه برای اینکه assets ساخته بشه
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx'
+      },
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   }
 })
