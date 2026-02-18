391fb47b0907fae4456ccb2757de8b234cc1b7a1
captainmop <captainmop@mop.captainmop>
2026-02-18 10:21:13 -0500

    Fix project structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,9 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { LanguageProvider } from "./context/LanguageContext";
 import "../styles/index.css";
+import { HashRouter, Routes, Route } from 'react-router-dom'
+// به جای RouterProvider یا BrowserRouter
+
 
 export default function App() {
   return (
