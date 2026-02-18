4009c3efa3fce7715078b858f59a9826a4b8e6e5
captainmop <captainmop@mop.captainmop>
2026-02-18 15:21:37 -0500

    fix mime types

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/1/',
+  base: './',  // تغییر به ./ به جای /1/
   server: {
     port: 5173,
     open: true
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   }
 })
