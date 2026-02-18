9fa93633906a82a5a52f8a4ce9135e506aa15082
captainmop <captainmop@mop.captainmop>
2026-02-18 15:27:26 -0500

    deploy dist

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // تغییر به ./ به جای /1/
+  base: './',
   server: {
     port: 5173,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
   }
 })
