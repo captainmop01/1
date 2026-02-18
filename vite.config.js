7cb553e2f203dda915fbe6105d19a1f2045efc4c
captainmop <captainmop@mop.captainmop>
2026-02-18 15:54:26 -0500

    deploy source files

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   server: {
     port: 5173,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
   }
 })
